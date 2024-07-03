e86fe6dd9c39a0cd064b8c65786af3215ce9f4f1
Felipspp <felipeprates49@gmail.com>
2024-07-03 02:04:33 -0300

    Pegando os modelos de forma dinámica

--- RealityKitCurso/ContentView.swift
+++ RealityKitCurso/ContentView.swift
@@ -8,9 +8,37 @@
 import SwiftUI
 import RealityKit
 
-struct ContentView : View {
-    var body: some View {
-        Text("Hello World")
+struct ContentView : View
+{
+    
+    private var models:[String] = {
+       // Pegar o nome dos modelos de forma dinamica
+        
+        let filemanager = FileManager.default
+        guard let path = Bundle.main.resourcePath, let files = try? filemanager.contentsOfDirectory(atPath: path) else {
+            return []
+        }
+        
+        var availableModels: [String] = []
+        
+        for file in files where file.hasSuffix("usdz") {
+            let modelName = file.replacingOccurrences(of: ".usdz", with: "")
+            
+            availableModels.append(modelName)
+        }
+        
+        return availableModels
+        
+    }()
+    
+    var body: some View
+    {
+        ZStack(alignment: .bottom)
+        {
+            ARViewContainer()
+            ModelPickerView(models: models)
+            
+        }
     }
 }
 
@@ -28,6 +56,32 @@ struct ARViewContainer: UIViewRepresentable {
     
     func updateUIView(_ uiView: ARView, context: Context) {}
     
+}
+
+struct ModelPickerView: View {
+    
+    var models:[String]
+    let size:CGFloat = 80
+    
+    var body: some View {
+        HStack(spacing: 10)
+        {
+            ForEach(0 ..< models.count, id: \.self)
+            {
+                index in
+                
+                Button {} label: {
+                    
+                    Image(models[index])
+                        .resizable()
+                        .frame(minWidth: size, maxWidth: size, minHeight: size, maxHeight: size)
+                        .aspectRatio(1/1, contentMode: .fit)
+                }
+                
+            }
+        Spacer()
+    }
+    }
 }
 
 #Preview {
